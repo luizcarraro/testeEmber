16fe23c279c981894de810fa0cfefe47a5c98cd6
Luiz Carraro <luiz.carraro@viasoft.com.br>
2017-01-03 08:12:56 -0200

    Muda nomes para singular

---
+++ Sails/api/models/Cliente.js
@@ -0,0 +1,20 @@
+/**
+ * Clientes.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    	nome:"string",
+  	sobrenome:"string"	
+  },  
+  associations: {
+      list: "link",
+      detail: "record"
+  }
+
+};
+
